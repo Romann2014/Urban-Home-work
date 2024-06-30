651b05ffb2a53dbd3fa6e726dfc607c88af7c47d
Romann2014 <toropcev_rs@mail.ru>
2024-06-30 22:30:51 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,47 @@
+from statistics import mean
+
+print ("5")
+number=5
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print ("Hello world")
+
+a=12345
+print ("12345")
+print (len ("12345"))
+
+first=10
+second=4
+print(10+4)
+print(10-4)
+
+first=10
+second=9
+third=8
+import statistics
+numbers = [10,9,8,]
+average=statistics.mean(numbers)
+print(average)
+
+first_string="Вторник"
+second_string="Понедельник"
+print("Понедельник, Вторник")
+
+a=5
+b=6
+c=7
+print((5*6)+(6*7))
+f=72
+print((72**3)//2)
